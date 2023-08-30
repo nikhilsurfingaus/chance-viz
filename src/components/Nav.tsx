c87313f027387d7399279ae63376a1f67e64f0e6
Nikhil Naik <46309727+nikhilsurfingaus@users.noreply.github.com>
2023-08-30 17:40:35 +1000

    Most Navbar done

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,26 +3,35 @@ import { NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/logo.png'
 
-import './Nav.css'; // Import your CSS file
+import './Nav.css';
 
 const NavTab = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">Chance Viz</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="text-center"> {/* Center the nav links */}
-          <Nav className="mx-auto"> {/* Horizontally center the nav links */}
-            <NavLink to="/" className="nav-link" end>
-              Home
-            </NavLink>
-            <NavLink to="/visual" className="nav-link">
-              Visualise
-            </NavLink>
-            <NavLink to="/about" className="nav-link">
-              About
-            </NavLink>
+      <Navbar.Brand className='text-white' href="/">
+          <span style={{ fontSize: '2rem' }}>Chance Viz</span>
+          <img src={logo} alt="Logo" className="logo-image" />
+        </Navbar.Brand>        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+          <Nav>
+            <div className="nav-link-container">
+              <NavLink to="/" className="nav-link" end>
+                Home
+              </NavLink>
+            </div>
+            <div className="nav-link-container">
+              <NavLink to="/visual" className="nav-link">
+                Visualise
+              </NavLink>
+            </div>
+            <div className="nav-link-container">
+              <NavLink to="/about" className="nav-link">
+                About
+              </NavLink>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
