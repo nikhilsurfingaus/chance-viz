715dfa2014680719c1f470eca5b8f69b0fc3304e
Nikhil Naik <46309727+nikhilsurfingaus@users.noreply.github.com>
2023-09-04 14:29:17 +1000

    Home Page Full UI

--- src/components/HomeSummary.tsx
+++ src/components/HomeSummary.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import './Summary.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import people from '../animation/rocket.json';
+import Lottie from 'lottie-react';
 
 const HomeSummary = () => {
   return (
-    <div style={{height: '100vh'}}>HomeSummary</div>
-  )
-}
+    <div className="about-inspire" style={{ backgroundColor: 'white' }}>
+      <Container>
+        <Row>
+          <Col md={7} xs={12} className="text-container">
+            <h1 className="inspire-heading">OUR MISSION...</h1>
+            <p className="lorem-ipsum">
+            At Chance Viz, our vision is to simplify the understanding of probabilities through captivating visuals, empowering individuals to make informed decisions and navigate life's uncertainties with confidence. We aim to provide a unique perspective that demystifies chance and enhances critical thinking.
+            </p>
+          </Col>
+          <Col md={5} xs={12} className="image-container">
+            <div className="blue-circle">
+              <Lottie animationData={people} style={{ height: '120%' }} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default HomeSummary
+export default HomeSummary;
