35614daf0abe22bb8e176e176b054b331235eb06
Nikhil Naik <46309727+nikhilsurfingaus@users.noreply.github.com>
2023-09-13 17:34:34 +1000

    Blurb Text full still issues showing blurb, need respo mobile dev need bug fixes blue overflow on 100M edge cases

--- src/components/VizInput.tsx
+++ src/components/VizInput.tsx
@@ -60,13 +60,14 @@ import milky from '../animation/visAnimate/galaxy.json'
 import atom from '../animation/visAnimate/atom.json'
 import cosmos from '../animation/visAnimate/cosmos.json'
 
-const ratioData = [
+const ratioBlurbData = [
   {
     ratio: 1,
     left: tennis,
     leftText: 'Tennis Ball',
     right: orange,
     rightText: 'Orange',
+    blurb: 'Your chance of around a 100% successful outcome is similar in size of a Tennis Ball (You) to an Orange (Total). Both a Tennis Ball and Orange is typically about 6.7cm in diameter. They are small enough to fit comfortably in your hand.'
   },
   {
     ratio: 0.75,
@@ -74,6 +75,7 @@ const ratioData = [
     leftText: 'Car',
     right: van,
     rightText: 'Van SUV',
+    blurb: 'Your chance of around a 75% successful outcome is similar in size of a Car (You) to a Van (Total). A Car is 4.8m in length. A Van SUV, in contrast, is larger and can be around 5.5m length.'
   },
   {
     ratio: 0.50,
@@ -81,6 +83,7 @@ const ratioData = [
     leftText: 'Soccer Field',
     right: nfl,
     rightText: 'NFL Field',
+    blurb: 'Your chance of around a 50% successful outcome is similar in size of a Soccer Field (You) to an NFL Field (Total). For perspective you need 2 Soccer Fields to be the same size as an NFL Field.'
   },
     {
     ratio: 0.25,
@@ -88,6 +91,7 @@ const ratioData = [
     leftText: 'Grape',
     right: burger,
     rightText: 'Burger',
+    blurb: 'Your chance of around a 25% successful outcome is similar in size of a Grape (You) to a Burger (Total). For perspective you need 4 Grapes to be the same size as a Burger.'
   },
     {
     ratio: 0.10,
@@ -95,6 +99,7 @@ const ratioData = [
     leftText: 'Pencil',
     right: bottle,
     rightText: 'Bottle',
+    blurb: 'Your chance of around a 10% successful outcome is similar in size of a Pencil (You) to a Bottle (Total). A Pencil is typically around 18cm in length and about 0.7cm in width. A bottle,  is typically about 20cm in height and 7cm in width. For perspective you need 10 Pencils to be the same size as a Bottle.'
   },
     {
     ratio: 0.05,
@@ -102,6 +107,7 @@ const ratioData = [
     leftText: 'Marble',
     right: bowling,
     rightText: 'Bowling Ball',
+    blurb: 'Your chance of around a 5% successful outcome is similar in size of a Marble (You) to a Bowling Ball (Total). A Marble is a small spherical toy, usually measuring about 1.5 cm in diameter. It is used in various games and activities. A Bowling Ball, used in the sport of bowling, is much larger, typically around 21cm in diameter and can weigh up to 7kg. For perspective you need 20 Marbles to be the same size as a Bowling Ball.'
   },
   {
     ratio: 0.01,
@@ -109,6 +115,7 @@ const ratioData = [
     leftText: 'Dolphin',
     right: sub,
     rightText: 'Submarine',
+    blurb: 'Your chance of around a 1% successful outcome is similar in size of a Dolphin (You) to a Submarine (Total).A Dolphin measures around 4m in length. A Submarine, a watercraft designed for underwater operations reaches around 170m in length. For perspective you need 100 Dolphins to be the same size as a Submarine.'
   },
     {
     ratio: 0.005,
@@ -116,20 +123,23 @@ const ratioData = [
     leftText: 'Phone Screen',
     right: cinema,
     rightText: 'Theatre Screen',
+    blurb: 'Your chance of around a 0.5% successful outcome is similar in size of a Phone Screen (You) to a Theatre Screen (Total). A Phone Screen, found on most smartphones, can vary in size but is typically around 15cm in diagonal width. A Theatre Screen, used in cinemas and theaters, can be quite large, with some screens measuring over 10m in width. For perspective you need minimum 200 Phone Screens to be the same size as a Theatre Screen.'
   },
     {
     ratio: 0.0001,
     left: cell,
     leftText: 'Singular Cell',
     right: human,
-    rightText: 'Human Body',
+    rightText: 'Human Hair',
+    blurb: 'Your chance of around a 0.01% successful outcome is similar in size of a Singular Cell (You) to Human Hair (Total). A Singular Cell, the basic unit of life, is incredibly tiny, typically measuring only a few micrometers (microns) in size, which is about 0.0001cm. Human Hair strands are much larger, averaging around 50 to 100 micrometers (0.005 to 0.01 cm) in diameter. For perspective you need at least 10,000 Cells to be the same size as Human Hair.'
   },
   {
     ratio: 0.00001,
     left: sand,
     leftText: 'Grain Of Sand',
     right: beach,
-    rightText: 'Beach',
+    rightText: 'Barrel of Sand',
+    blurb: 'Your chance of around a 0.001% successful outcome is similar in size of a Grain Of Sand (You) in a Barrel of Sand (Total). A Grain Of Sand is a minuscule particle, measuring around 0.05cm in diameter. A Barrel of Sand, containing countless grains of sand has dimensions of around 90cm height and 60cm in diameter. For perspective you need at least 100,000 Grains of Sand to be the same size as the Barrel of Sand.'
   },
   {
     ratio: 0.000001,
@@ -137,6 +147,7 @@ const ratioData = [
     leftText: 'Single Molecule',
     right: droplet,
     rightText: 'Water Droplet',
+    blurb: 'Your chance of around a 0.0001% successful outcome is similar in size of a Single Molecule (You) in a Water Droplet (Total). A Single Molecule is incredibly small, often less than a nanometer 0.0000001cm in size. A Water Droplet can vary in size, but a typical small droplet might have a diameter of around 0.1cm. For perspective you need at least 1,000,000 Molecules to be the same size as the Water Droplet.'
   },
   {
     ratio: 0.0000001,
@@ -144,6 +155,7 @@ const ratioData = [
     leftText: 'Total Coral',
     right: ocean,
     rightText: 'Total Ocean',
+    blurb: 'Your chance of around a 0.00001% successful outcome is similar in size of all the Coral Reef (You) in the Worlds Ocean (Total). Total Coral, referring to the entire coral reef ecosystem, covers various sizes and shapes, but it is much smaller compared to the vastness of the ocean. The Worlds Ocean, covers approximately 361 million square kilometers, or 36,100,000,000,000,000,000,000 cm^2 in surface area. For perspective you need at least 10,000,000 Coral Reefs to be the same size as the Globes Ocean.'
   },
   {
     ratio: 50,
@@ -151,6 +163,7 @@ const ratioData = [
     leftText: 'Single Star',
     right: milky,
     rightText: 'Milky Way Galaxy',
+    blurb: 'Your chance of around a 0.000002% successful outcome is similar in size of a Single Star (You) in the Milky Way Galaxy (Total). A Single Star, like our Sun, is a massive celestial object but relatively small on a cosmic scale. Its typically a few hundred thousand kilometers (km) in diameter. The Milky Way Galaxy, our home galaxy, is enormous, with a diameter of about 100,000 light-years, equivalent to approximately 9.46 x 10^17 kilometers (km).For perspective you need at least 50,000,000 Stars to be the same size as the Milky Way Galaxy.'
   },
   {
     ratio: 0.000000025,
@@ -158,6 +171,123 @@ const ratioData = [
     leftText: 'Singular Atom',
     right: cosmos,
     rightText: 'Cosmos Universe',
+    blurb: 'Your chance of around less than 0.000002% of a successful outcome is similar in size of a Single Atom (You) in the Cosmos Universe (Total). A Singular Atom is the tiniest unit of matter, with a size on the order of picometers 0.00000000001cm. The Cosmos Universe encompasses the entire known universe, including countless galaxies. Its size is estimated to be about 93 billion light-years in diameter, or roughly 8.8 x 10^26 kilometers (km). For perspective you need at least 100,000,000 Atoms to be the same size as the Cosmos Universe.'
+  },
+  // Add more ratio objects as needed
+];
+
+const ratioData = [
+  {
+    ratio: 1,
+    left: tennis,
+    leftText: 'Tennis Ball',
+    right: orange,
+    rightText: 'Orange',
+    blurb: 'Your chance of around a 100% successful outcome is similar in size of a Tennis Ball (You) to an Orange (Total). Both a Tennis Ball and Orange is typically about 6.7cm in diameter. They are small enough to fit comfortably in your hand.'
+  },
+  {
+    ratio: 0.75,
+    left: car,
+    leftText: 'Car',
+    right: van,
+    rightText: 'Van SUV',
+    blurb: 'Your chance of around a 75% successful outcome is similar in size of a Car (You) to a Van (Total). A Car is 4.8m in length. A Van SUV, in contrast, is larger and can be around 5.5m length.'
+  },
+  {
+    ratio: 0.50,
+    left: soccer,
+    leftText: 'Soccer Field',
+    right: nfl,
+    rightText: 'NFL Field',
+    blurb: 'Your chance of around a 50% successful outcome is similar in size of a Soccer Field (You) to an NFL Field (Total). For perspective you need 2 Soccer Fields to be the same size as an NFL Field.'
+  },
+    {
+    ratio: 0.25,
+    left: grape,
+    leftText: 'Grape',
+    right: burger,
+    rightText: 'Burger',
+    blurb: 'Your chance of around a 25% successful outcome is similar in size of a Grape (You) to a Burger (Total). For perspective you need 4 Grapes to be the same size as a Burger.'
+  },
+    {
+    ratio: 0.10,
+    left: pencil,
+    leftText: 'Pencil',
+    right: bottle,
+    rightText: 'Bottle',
+    blurb: 'Your chance of around a 10% successful outcome is similar in size of a Pencil (You) to a Bottle (Total). A Pencil is typically around 18cm in length and about 0.7cm in width. A bottle,  is typically about 20cm in height and 7cm in width. For perspective you need 10 Pencils to be the same size as a Bottle.'
+  },
+    {
+    ratio: 0.05,
+    left: marble,
+    leftText: 'Marble',
+    right: bowling,
+    rightText: 'Bowling Ball',
+    blurb: 'Your chance of around a 5% successful outcome is similar in size of a Marble (You) to a Bowling Ball (Total). A Marble is a small spherical toy, usually measuring about 1.5 cm in diameter. It is used in various games and activities. A Bowling Ball, used in the sport of bowling, is much larger, typically around 21cm in diameter and can weigh up to 7kg. For perspective you need 20 Marbles to be the same size as a Bowling Ball.'
+  },
+  {
+    ratio: 0.01,
+    left: dolphin,
+    leftText: 'Dolphin',
+    right: sub,
+    rightText: 'Submarine',
+    blurb: 'Your chance of around a 1% successful outcome is similar in size of a Dolphin (You) to a Submarine (Total).A Dolphin measures around 4m in length. A Submarine, a watercraft designed for underwater operations reaches around 170m in length. For perspective you need 100 Dolphins to be the same size as a Submarine.'
+  },
+    {
+    ratio: 0.005,
+    left: phone,
+    leftText: 'Phone Screen',
+    right: cinema,
+    rightText: 'Theatre Screen',
+    blurb: 'Your chance of around a 0.5% successful outcome is similar in size of a Phone Screen (You) to a Theatre Screen (Total). A Phone Screen, found on most smartphones, can vary in size but is typically around 15cm in diagonal width. A Theatre Screen, used in cinemas and theaters, can be quite large, with some screens measuring over 10m in width. For perspective you need minimum 200 Phone Screens to be the same size as a Theatre Screen.'
+  },
+    {
+    ratio: 0.0001,
+    left: cell,
+    leftText: 'Singular Cell',
+    right: human,
+    rightText: 'Human Hair',
+    blurb: 'Your chance of around a 0.01% successful outcome is similar in size of a Singular Cell (You) to Human Hair (Total). A Singular Cell, the basic unit of life, is incredibly tiny, typically measuring only a few micrometers (microns) in size, which is about 0.0001cm. Human Hair strands are much larger, averaging around 50 to 100 micrometers (0.005 to 0.01 cm) in diameter. For perspective you need at least 10,000 Cells to be the same size as Human Hair.'
+  },
+  {
+    ratio: 0.00001,
+    left: sand,
+    leftText: 'Grain Of Sand',
+    right: beach,
+    rightText: 'Barrel of Sand',
+    blurb: 'Your chance of around a 0.001% successful outcome is similar in size of a Grain Of Sand (You) in a Barrel of Sand (Total). A Grain Of Sand is a minuscule particle, measuring around 0.05cm in diameter. A Barrel of Sand, containing countless grains of sand has dimensions of around 90cm height and 60cm in diameter. For perspective you need at least 100,000 Grains of Sand to be the same size as the Barrel of Sand.'
+  },
+  {
+    ratio: 0.000001,
+    left: molecule,
+    leftText: 'Single Molecule',
+    right: droplet,
+    rightText: 'Water Droplet',
+    blurb: 'Your chance of around a 0.0001% successful outcome is similar in size of a Single Molecule (You) in a Water Droplet (Total). A Single Molecule is incredibly small, often less than a nanometer 0.0000001cm in size. A Water Droplet can vary in size, but a typical small droplet might have a diameter of around 0.1cm. For perspective you need at least 1,000,000 Molecules to be the same size as the Water Droplet.'
+  },
+  {
+    ratio: 0.0000001,
+    left: coral,
+    leftText: 'Total Coral',
+    right: ocean,
+    rightText: 'Total Ocean',
+    blurb: 'Your chance of around a 0.00001% successful outcome is similar in size of all the Coral Reef (You) in the Worlds Ocean (Total). Total Coral, referring to the entire coral reef ecosystem, covers various sizes and shapes, but it is much smaller compared to the vastness of the ocean. The Worlds Ocean, covers approximately 361 million square kilometers, or 36,100,000,000,000,000,000,000 cm^2 in surface area. For perspective you need at least 10,000,000 Coral Reefs to be the same size as the Globes Ocean.'
+  },
+  {
+    ratio: 50,
+    left: star,
+    leftText: 'Single Star',
+    right: milky,
+    rightText: 'Milky Way Galaxy',
+    blurb: 'Your chance of around a 0.000002% successful outcome is similar in size of a Single Star (You) in the Milky Way Galaxy (Total). A Single Star, like our Sun, is a massive celestial object but relatively small on a cosmic scale. Its typically a few hundred thousand kilometers (km) in diameter. The Milky Way Galaxy, our home galaxy, is enormous, with a diameter of about 100,000 light-years, equivalent to approximately 9.46 x 10^17 kilometers (km).For perspective you need at least 50,000,000 Stars to be the same size as the Milky Way Galaxy.'
+  },
+  {
+    ratio: 0.000000025,
+    left: atom,
+    leftText: 'Singular Atom',
+    right: cosmos,
+    rightText: 'Cosmos Universe',
+    blurb: 'Your chance of around less than 0.000002% of a successful outcome is similar in size of a Single Atom (You) in the Cosmos Universe (Total). A Singular Atom is the tiniest unit of matter, with a size on the order of picometers 0.00000000001cm. The Cosmos Universe encompasses the entire known universe, including countless galaxies. Its size is estimated to be about 93 billion light-years in diameter, or roughly 8.8 x 10^26 kilometers (km). For perspective you need at least 100,000,000 Atoms to be the same size as the Cosmos Universe.'
   },
   // Add more ratio objects as needed
 ];
@@ -317,15 +447,13 @@ const VizInput = () => {
               if (ratio === ratioItem.ratio) {
                 return (
                   <div key={ratioItem.ratio} style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }} className='sids'>
-                    <div>
-                    </div>
                     <div style={{ flex: 1, background: 'rgba(255, 255, 255, 0.93)' }}>
                       <Lottie animationData={ratioItem.left} style={{ height: '400px' }} />
-                      <p style={{fontSize: '2.5rem', fontWeight: 'bold'}}>{ratioItem.leftText}</p>
+                      <p style={{fontSize: '2.5rem', fontWeight: 'bold'}}>{ratioItem.leftText} (You)</p>
                     </div>
                     <div style={{ flex: 1, background: '#1479ed', color: 'white', borderRadius: '15px'}}>
                       <Lottie className='ani' animationData={ratioItem.right} style={{ height: '400px'}} />
-                      <p style={{fontSize: '2.5rem', fontWeight: 'bold'}}>{ratioItem.rightText}</p>
+                      <p style={{fontSize: '2.5rem', fontWeight: 'bold'}}>{ratioItem.rightText} (Total)</p>
                     </div>
                   </div>
                 );
@@ -334,6 +462,18 @@ const VizInput = () => {
             })}
           </div>
           <div className='stats mt-5'>
+            {/* <div className='w-50'>
+            {ratioBlurbData.map((ratioItem) => {
+                if (ratio === ratioItem.ratio) {
+                  return ( 
+                    <div>
+                      <p style={{ fontSize: '1.5rem' }}>{ratioItem.blurb}</p>
+                    </div>
+                  )
+                }
+                return null
+              })}
+            </div> */}
             <button onClick={handleSwitch}>Back to Input</button>
             <div>Result</div>
             <p>Chance Ratio: {ratio}</p>
